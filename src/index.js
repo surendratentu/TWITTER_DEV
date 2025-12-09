85b8569b0febae818b8220806fe7a8540c2d0795
surendratentu <surendratentu2005@gmail.com>
2025-12-09 10:19:34 +0530

    Add src as normal folder

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const connectDB = require('./config/database');
+const app = express();
+
+const TweetRepository = require('./repository/tweet-repository');
+const Comment = require('./models/comment');
+
+app.listen(3000, async () => {
+    console.log('Server started');
+    await connectDB();
+    console.log('Mongo DB connected');
+    // const tweet =  await Tweet.create({
+    //     content: 'second tweet'
+    // });
+    // const tweets = await Tweet.find({});
+    // console.log(tweets);
+
+    const tweetRepo = new TweetRepository();
+    // const tweet = await tweetRepo.getAll(0, 4);
+    // console.log(tweet[0].contentWithEmail);
+    const tweet = await tweetRepo.create({ content: 'With hooks now' });
+    console.log(tweet)
+
+});
