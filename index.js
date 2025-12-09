95ac1c929d3c120ea80fcebcd45a9bdb3079743c
surendratentu <surendratentu2005@gmail.com>
2025-12-09 12:43:59 +0530

    Started adding create service for tweets

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const connectDB = require('./config/database');
-const app = express();
-
-app.listen(3000, async () => {
-    console.log('Server started');
-    await connectDB();
-    console.log('Mongo DB connected');
-});
